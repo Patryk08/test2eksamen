0df9c42a53f6168c24e49c50d9d7c0423589df59
Patryk08 <patrykmistak@gmail.com>
2026-04-29 15:48:16 +0200

    Update Eksammenn.py

--- Eksammenn.py
+++ Eksammenn.py
@@ -11,4 +11,4 @@ def projects():
     return render_template('projects.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5808)
