4128613dd306dbe9248835f69846b831f5654334
Patryk08 <patrykmistak@gmail.com>
2026-04-29 15:23:48 +0200

    Dette er det jeg har gjort til nå!

--- Eksammenn.py
+++ Eksammenn.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5808)
+    app.run(debug=True, host='0.0.0.0', port=5000)
